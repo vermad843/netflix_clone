2576f935473ec7780037688f42320ab410ffc0a5
vermad843 <vishnuverma7524@gmail.com>
2021-02-18 03:16:57 +0530

    images on page

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,9 @@
 import React,{useState, useEffect} from 'react';
 import axios from './axios';
+import './Row.css';
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
@@ -7,15 +11,23 @@ function Row({title, fetchUrl}) {
     useEffect(() => {
       async function fetchData() {
           const request = await axios.get(fetchUrl);
-          console.log(request);
           setMovies(request.data.results);
           return request;
       }
       fetchData();
     }, [fetchUrl]);
+
+
     return (
-        <div>
+        <div className = "row">
             <h2>{title}</h2>
+            <div className = "row__posters">
+                {movies.map(movie => (
+                    <img 
+                     className = "row__poster"
+                     src = {`${base_url}${movie.poster_path}`} alt = {movie.name}/>
+                ))}
+            </div>
         </div>
     );
 }
